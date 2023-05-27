9d21369911351464644f1fd7f513c35854588875
Usama Munir <usama_1s@hotmail.com>
2023-05-28 04:28:04 +0500

    added sliders

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -1,10 +1,103 @@
 import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+import sliderImg1 from "../assets/images/Slider-image-01.jpg";
+import sliderImg2 from "../assets/images/Slider-image-02.jpg";
+import sliderImg3 from "../assets/images/Slider-image-03.jpg";
+import sliderImg4 from "../assets/images/Slider-image-04.jpg";
+import sliderImg5 from "../assets/images/Slider-image-05.jpg";
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 1,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+
+const customDotText = [
+  "Insider Transactions Tracking",
+  "Fundamentals",
+  "FDA Stocks",
+  "Congress Traded Transactions",
+  "Hedge Funds Analysis",
+];
+
+let sliders = [];
+
+sliders.push(
+  <div className="img">
+    <img src={sliderImg1} />
+  </div>
+);
+sliders.push(
+  <div className="img">
+    <img src={sliderImg2} />
+  </div>
+);
+sliders.push(
+  <div className="img">
+    <img src={sliderImg3} />
+  </div>
+);
+sliders.push(
+  <div className="img">
+    <img src={sliderImg4} />
+  </div>
+);
+sliders.push(
+  <div className="img">
+    <img src={sliderImg5} />
+  </div>
+);
+
+
+const CustomDot = ({ onMove, index, onClick, active }) => {
+  // onMove means if dragging or swiping in progress.
+  // active is provided by this lib for checking if the item is active or not.
+  return (
+    <li className={active ? "active" : "inactive"} onClick={() => onClick()}>
+      {customDotText[index]}
+      &nbsp;
+      <span>{index+1 + "/" + sliders.length}</span>
+    </li>
+  );
+};
 
 const Section1 = () => {
   return (
-    <section class="container">
-      <div>
-        <div class="img-slider"></div>
+    <section className=" imgs-slider">
+      <div className="">
+        <Carousel
+          responsive={responsive}
+          swipeable={true}
+          draggable={true}
+          showDots={true}
+          ssr={true} // means to render carousel on server-side.
+          infinite={true}
+          autoPlaySpeed={1000}
+          keyBoardControl={true}
+          transitionDuration={500}
+          containerClass="carousel-container"
+          dotListClass="custom-dot-list-style"
+          itemClass="carousel-item-padding-40-px"
+          customDot={<CustomDot />}
+        >
+          {sliders}
+        </Carousel>
       </div>
     </section>
   );
--- src/components/Section3.jsx
+++ src/components/Section3.jsx
@@ -1,4 +1,117 @@
 import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 1,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+
+let sliders = [];
+
+sliders.push(
+  <div className="question-box">
+    <span>1 of 5</span>
+    <h3>Do you want to see how Nancy Pelosi has been trading the market? </h3>
+    <button onClick={() => nextSlideCustom()}>
+      Yes
+    </button>
+    <button onClick={() => nextSlideCustom()}>
+      No
+    </button>
+  </div>
+);
+
+sliders.push(
+  <div className="question-box">
+    <span>2 of 5</span>
+    <h3>Do you want a breakdown of stocks based off their financial ratios? </h3>
+    <button onClick={() => nextSlideCustom()}>
+      Yes
+    </button>
+    <button onClick={() => nextSlideCustom()}>
+      No
+    </button>
+  </div>
+);
+
+sliders.push(
+  <div className="question-box">
+    <span>3 of 5</span>
+    <h3>Do you want to see the portfolio composition of the biggest hedge funds? </h3>
+    <button onClick={() => nextSlideCustom()}>
+      Yes
+    </button>
+    <button onClick={() => nextSlideCustom()}>
+      No
+    </button>
+  </div>
+);
+
+sliders.push(
+  <div className="question-box">
+    <span>4 of 5</span>
+    <h3>Do you want to see which stocks have the most insider trading activity? </h3>
+    <button onClick={() => nextSlideCustom()}>
+      Yes
+    </button>
+    <button onClick={() => nextSlideCustom()}>
+      No
+    </button>
+  </div>
+);
+
+sliders.push(
+  <div className="question-box">
+    <span>5 of 5</span>
+    <h3>Do you want to combine all of this data? </h3>
+    <button onClick={() => nextSlideCustom()}>
+      Yes
+    </button>
+    <button onClick={() => nextSlideCustom()}>
+      No
+    </button>
+  </div>
+);
+
+sliders.push(
+  <div className="question-box question-box-last">
+    <h3>Seems like StockAlgos is suitable to you. <br /> Would you like to give us a try? </h3>
+    <button className="violet-fill-btn">
+      Try at $0 for 7 days
+    </button>
+    <p class="small-text"><s>$214.00</s> $99/month after</p>
+  </div>
+);
+
+
+
+const nextSlideCustom = () =>{
+  document.getElementById('custom-right-btn').click();
+}
+
+const CustomRight = ({ onClick }) => {
+  return (
+  <button id="custom-right-btn" onClick={onClick}>
+    
+  </button>
+  );
+};
 
 const Section3 = () => {
   return (
@@ -9,7 +122,27 @@ const Section3 = () => {
           Answer some questions to learn if you can be benefited from StockAlgos
           Unlimited.
         </p>
-        <div class="question-slider"></div>
+        <div class="question-slider">
+
+        <Carousel
+          responsive={responsive}
+          swipeable={true}
+          draggable={true}
+          showDots={false}
+          ssr={true} // means to render carousel on server-side.
+          infinite={false}
+          autoPlaySpeed={1000}
+          keyBoardControl={true}
+          transitionDuration={500}
+          containerClass="carousel-container"
+          dotListClass="custom-dot-list-style"
+          itemClass="carousel-item-padding-40-px"
+          customRightArrow={<CustomRight />}
+   
+        >
+          {sliders}
+        </Carousel>
+        </div>
       </div>
     </section>
   );
